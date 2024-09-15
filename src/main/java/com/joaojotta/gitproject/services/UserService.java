dbba4b2b4aa107a62b77fe5188286ebc79c39ef9
JoaoSantos6 <78502928+JoaoSantos6@users.noreply.github.com>
2024-09-15 12:17:01 -0300

    Service layer, component registration

---
+++ src/main/java/com/joaojotta/gitproject/services/UserService.java
@@ -0,0 +1,26 @@
+package com.joaojotta.gitproject.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.joaojotta.gitproject.entities.User;
+import com.joaojotta.gitproject.repositories.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository;
+
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+}
